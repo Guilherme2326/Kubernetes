fddbd13279a9b70abbeb2b06a75e33a0d5b45564
guilherme <guilhermeguiguilima001@gmail.com>
2025-02-12 12:14:57 -0300

    adiciona Dockerfile e servidor HTTP em Go, removendo código de soma, cria imagem docker

---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// func main() {
+// 	err := http.ListenAndServe(":80", nil)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	http.HandleFunc("/", Hello)
+// 	http.ListenAndServe(":8080", nil)
+// }
+
+// func Hello(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("<h1>Hello, World!</h1>"))
+// }
+
+func main() {
+    // Registra um handler para a rota raiz ("/")
+	
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintln(w, "Olá, Mundo!")
+		w.Write([]byte("<h1>Hello, World!</h1>"))
+// }
+    })
+
+    // Inicia o servidor na porta 8080 (ou outra porta não privilegiada)
+    fmt.Println("Servidor iniciado na porta 8080")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+	log.Fatal(err)
+	}
+}

c793e075f3481f9bf52289513048a2d083438929
Guilherme <guilhermeguiguilima001@gmail.com>
2025-02-13 13:06:36 -0300

    atualiza imagem do goserver para v3, adiciona ConfigMap para variáveis de ambiente e ajusta handler para exibir informações do usuário

--- server.go
+++ server.go
@@ -2,36 +2,20 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 )
 
-// func main() {
-// 	err := http.ListenAndServe(":80", nil)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	http.HandleFunc("/", Hello)
-// 	http.ListenAndServe(":8080", nil)
-// }
-
-// func Hello(w http.ResponseWriter, r *http.Request) {
-// 	w.Write([]byte("<h1>Hello, World!</h1>"))
-// }
-
 func main() {
-    // Registra um handler para a rota raiz ("/")
-	
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Olá, Mundo!")
-		w.Write([]byte("<h1>Hello, World!!!!</h1>"))
-// }
-    })
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8000", nil)
+}
 
-    // Inicia o servidor na porta 8080 (ou outra porta não privilegiada)
-    fmt.Println("Servidor iniciado na porta 8080")
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-	log.Fatal(err)
-	}
+func Hello(w http.ResponseWriter, r *http.Request) {
+
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age)
+
 }
